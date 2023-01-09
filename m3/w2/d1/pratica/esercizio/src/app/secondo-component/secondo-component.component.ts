90019e31ed9578ae39a936cd198f200f54370ab9
Giacomo202 <giaby202@gmail.com>
2023-01-09 17:07:39 +0100

    m3w2d1pratica

---
+++ m3/w2/d1/pratica/esercizio/src/app/secondo-component/secondo-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondo-component',
+  templateUrl: './secondo-component.component.html',
+  styleUrls: ['./secondo-component.component.css']
+})
+export class SecondoComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
